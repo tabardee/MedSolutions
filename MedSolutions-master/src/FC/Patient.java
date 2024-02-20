2a5f6e211aafabcaa94c6a3fda43f0387b86e047
Pauline <pauline030101@gmail.com>
2024-02-20 17:43:35 +0100

    test

---
+++ MedSolutions-master/src/FC/Patient.java
@@ -0,0 +1,15 @@
+package FC;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pauli
+ */
+public abstract class Patient { 
+   
+}
